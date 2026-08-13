ec1b21459b19bc25cc6b7f480a3a39b38ab477ac
hiroko ishikawa <kiri310@fuga.ocn.ne.jp>
2026-08-14 00:12:10 +0900

    #2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// お問い合わせフォーム（公開）
+Route::get('/', [ContactController::class, 'index']);
+Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::post('/contacts', [ContactController::class, 'store']);
+Route::get('/thanks', [ContactController::class, 'thanks']);
+
+// 管理画面（認証必須）
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', [AdminController::class, 'index']);
+    Route::get('/admin/contacts/{contact}', [AdminController::class, 'show']);
+    Route::delete('/admin/contacts/{contact}', [AdminController::class, 'destroy']);
+    Route::get('/contacts/export', [ContactController::class, 'export']);
+    Route::post('/admin/tags', [TagController::class, 'store']);
+    Route::get('/admin/tags/{tag}/edit', [TagController::class, 'edit']);
+    Route::put('/admin/tags/{tag}', [TagController::class, 'update']);
+    Route::delete('/admin/tags/{tag}', [TagController::class, 'destroy']);
 });
